3f518a465ff1dcc285db45aad5e5a74cd3fcfa97
Evander Owusu <owu.evander@gmail.com>
2021-12-01 22:39:33 +0000

    give snake initials

--- client.js
+++ client.js
@@ -6,12 +6,17 @@ const connect = function () {
     host: "165.227.47.243",
     port: 50541,
   });
-
+  let name = "";
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("data", (message) => {
     console.log("server sent:", message);
+  });
+
+  conn.on("connect", () => {
+    console.log("successfully connected to the game server");
+    conn.write("Name: KOF");
   });
 
   return conn;
